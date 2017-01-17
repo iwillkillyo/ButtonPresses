fa6fd7c1f2f68c6d7ed5f5799756a86dde5b3d04
iwillkillyo <plenkhost@gmail.com>
2017-01-17 18:41:09 +0100

    Commit #1

---
+++ ButtonPresses/Form1.Designer.cs
@@ -0,0 +1,146 @@
+﻿namespace ButtonPresses
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.Item = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.Dukat = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.Platinum = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.backgroundWorker2 = new System.ComponentModel.BackgroundWorker();
+            this.progressBar2 = new System.Windows.Forms.ProgressBar();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(207, 57);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(176, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "On the relic reward selection screen";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(252, 103);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(97, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Press the END key";
+            // 
+            // listView1
+            // 
+            this.listView1.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.Item,
+            this.Dukat,
+            this.Platinum});
+            this.listView1.FullRowSelect = true;
+            this.listView1.Location = new System.Drawing.Point(54, 139);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(493, 132);
+            this.listView1.TabIndex = 3;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            // 
+            // Item
+            // 
+            this.Item.Text = "Item";
+            this.Item.Width = 375;
+            // 
+            // Dukat
+            // 
+            this.Dukat.Text = "Dukat";
+            // 
+            // Platinum
+            // 
+            this.Platinum.Text = "Platinum";
+            // 
+            // backgroundWorker1
+            // 
+            this.backgroundWorker1.DoWork += new System.ComponentModel.DoWorkEventHandler(this.backgroundWorker1_DoWork);
+            this.backgroundWorker1.RunWorkerCompleted += new System.ComponentModel.RunWorkerCompletedEventHandler(this.backgroundWorker1_RunWorkerCompleted);
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(192, 277);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(223, 23);
+            this.progressBar1.TabIndex = 4;
+            // 
+            // backgroundWorker2
+            // 
+            this.backgroundWorker2.DoWork += new System.ComponentModel.DoWorkEventHandler(this.backgroundWorker2_DoWork);
+            // 
+            // progressBar2
+            // 
+            this.progressBar2.Location = new System.Drawing.Point(192, 277);
+            this.progressBar2.Name = "progressBar2";
+            this.progressBar2.Size = new System.Drawing.Size(223, 23);
+            this.progressBar2.TabIndex = 5;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(605, 332);
+            this.Controls.Add(this.progressBar2);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.listView1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.ShowIcon = false;
+            this.Text = "Iwillkillyo\'s drop valuer";
+            this.Shown += new System.EventHandler(this.Form1_Shown);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private KeyHandler ghk;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.ColumnHeader Item;
+        private System.Windows.Forms.ColumnHeader Dukat;
+        private System.Windows.Forms.ColumnHeader Platinum;
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.ComponentModel.BackgroundWorker backgroundWorker2;
+        private System.Windows.Forms.ProgressBar progressBar2;
+    }
+}
+
---
+++ ButtonPresses/Form1.cs
@@ -0,0 +1,323 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Runtime.InteropServices;
+using System.Diagnostics;
+using System.Drawing.Imaging;
+using Tesseract;
+using System.Text.RegularExpressions;
+using System.IO;
+using System.Net;
+using System.Threading;
+
+namespace ButtonPresses
+{
+    public partial class Form1 : Form
+    {
+        string parsedString = "";
+        public static class Constants
+        {
+            //windows message id for hotkey
+            public const int WM_HOTKEY_MSG_ID = 0x0312;
+        }
+
+        public class Variables
+        {
+            public string parsedString;
+        }
+
+        /*
+         * Getting the pressed button
+         */
+
+        public class KeyHandler
+        {
+            [DllImport("user32.dll")]
+            private static extern bool RegisterHotKey(IntPtr hWnd, int id, int fsModifiers, int vk);
+
+            [DllImport("user32.dll")]
+            private static extern bool UnregisterHotKey(IntPtr hWnd, int id);
+
+            private int key;
+            private IntPtr hWnd;
+            private int id;
+
+            public KeyHandler(Keys key, Form form)
+            {
+                this.key = (int)key;
+                this.hWnd = form.Handle;
+                id = this.GetHashCode();
+            }
+
+            public override int GetHashCode()
+            {
+                return key ^ hWnd.ToInt32();
+            }
+
+            public bool Register()
+            {
+                return RegisterHotKey(hWnd, id, 0, key);
+                
+            }
+
+            public bool Unregiser()
+            {
+                return UnregisterHotKey(hWnd, id);
+            }
+        }
+
+        /*
+         * Handling the buttonpress
+         */
+
+        private void HandleHotkey()
+        {
+            if(!backgroundWorker2.IsBusy)
+            {
+                progressBar1.Visible = true;
+                listView1.Items.Clear();
+                if (CaptureApplication("Warframe.x64"))
+                {
+                    progressBar1.Value = 20;
+                    if (!backgroundWorker1.IsBusy)
+                    {
+                        progressBar1.Value = 50;
+                        backgroundWorker1.RunWorkerAsync();
+                        progressBar1.Value = 90;
+                    }
+                    else
+                    {
+                        //backgroundWorker1.CancelAsync();
+                    }
+                }
+            }
+            else
+            {
+                MessageBox.Show("Program is still checking versions!");
+            }
+        }
+
+        /*
+         * User 32 class
+         */
+
+        private class User32
+        {
+            [StructLayout(LayoutKind.Sequential)]
+            public struct Rect
+            {
+                public int left;
+                public int top;
+                public int right;
+                public int bottom;
+            }
+
+            [DllImport("user32.dll")]
+            public static extern IntPtr GetWindowRect(IntPtr hWnd, ref Rect rect);
+        }
+
+        /*
+         * Capturing the application
+         */
+
+        private bool CaptureApplication(string procName)
+        {
+            if(Process.GetProcessesByName("Warframe.x64") != null)
+            {
+                progressBar1.Visible = true;
+                int width = Convert.ToInt16(Screen.PrimaryScreen.Bounds.Width);
+                int height = Convert.ToInt16(Screen.PrimaryScreen.Bounds.Height);
+
+
+                var bmp = new Bitmap(width, height, PixelFormat.Format32bppArgb);
+                Graphics graphics = Graphics.FromImage(bmp);
+                graphics.CopyFromScreen(0, 0, 0, 0, new Size(width, height), CopyPixelOperation.SourceCopy);
+
+
+                string str = string.Format(Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments) + @"\Screenshot.tif");
+                try
+                {
+                    bmp.Save(str, System.Drawing.Imaging.ImageFormat.Tiff);
+                }
+                catch
+                {
+
+                }
+                return true;
+            }
+            else
+            {
+                MessageBox.Show("Warframe is not running!");
+                return false;
+            }
+        }
+
+        /*
+         * Dunno what is this
+         */
+
+        protected override void WndProc(ref Message m)
+        {
+            if (m.Msg == Constants.WM_HOTKEY_MSG_ID)
+                HandleHotkey();
+            base.WndProc(ref m);
+        }
+
+        /*
+         * Getting the drops
+         */
+
+        private void matchAgainsDrops(string text)
+        {
+            string[] drops = File.ReadAllLines("wf_drops.txt");
+            foreach (var item in drops)
+            {
+                var isIt = Regex.Match(text, item, RegexOptions.IgnoreCase);
+                if (isIt.Success)
+                {
+                    listView1.Items.Add(new ListViewItem(new string[] { item, "Dukat" }));
+                }
+            }
+        }
+
+        /*
+         * Extracting the string from the picture
+         */
+
+        private string getStringFromPic()
+        {
+            string pictureString = "";
+            var pict = string.Format(Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments) + @"\Screenshot.tif");
+            try
+            {
+                using (var engine = new TesseractEngine(@"./tessdata", "eng", EngineMode.Default))
+                {
+                    using (var img = Pix.LoadFromFile(pict))
+                    {
+                        using (var page = engine.Process(img))
+                        {
+                            var text = page.GetText();
+                            pictureString = text;
+                        }
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                Trace.TraceError(e.ToString());
+            }
+            parsedString = pictureString;
+            return pictureString;
+        }
+        public Form1()
+        {
+            InitializeComponent();
+            listView1.View = View.Details;
+            ghk = new KeyHandler(Keys.End, this);
+            ghk.Register();
+            backgroundWorker1.WorkerReportsProgress = true;
+            progressBar1.Visible = false;
+            progressBar2.Visible = false;
+        }
+
+        /*
+         * Get's the most update version's number [No updating here, just checking!]
+         */
+
+        public void getVersion()
+        {
+            if(progressBar2.InvokeRequired)
+            {
+                progressBar2.Invoke(new MethodInvoker(delegate { progressBar2.Visible = true; }));
+            }
+            WebClient client = new WebClient();
+            string version = "";
+            try
+            {
+                version = client.DownloadString(new Uri("http://188.240.208.209/text.txt"));
+                if (progressBar2.InvokeRequired)
+                {
+                    progressBar2.Invoke(new MethodInvoker(delegate { progressBar2.Value = 50; }));
+                }
+            }
+            catch (WebException e)
+            {
+                MessageBox.Show(e.Message + "\r\nUsing old database, so prices won't be correct!");
+            }
+            if(File.Exists("version.txt"))
+            {
+                string[] currVer = File.ReadAllLines("version.txt");
+                if (Regex.IsMatch(version, currVer[0]))
+                {
+                    MessageBox.Show("Program is up to date!");
+                }
+                else
+                {
+                    MessageBox.Show("There is a newer version of the program!");
+                }
+            }
+            else
+            {
+                File.Create("version.txt");
+                TextWriter tw = new StreamWriter("version.txt");
+                tw.Write("1.0.0");
+            }
+            if (progressBar2.InvokeRequired)
+            {
+                progressBar2.Invoke(new MethodInvoker(delegate { progressBar2.Visible = false; }));
+            }
+        }
+        
+
+        /*
+         * Background worker, so the form doesn't freeze
+         */
+
+        private void backgroundWorker1_DoWork(object sender, DoWorkEventArgs e)
+        {
+            getStringFromPic();
+            backgroundWorker1.ReportProgress(100);
+        }
+
+        private void backgroundWorker1_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
+        {
+            if(e.Cancelled)
+            {
+                // This can't happen but whatever
+            }
+            else if(e.Error != null)
+            {
+                MessageBox.Show("Error in runtime, try again.");
+            }
+            else
+            {
+                matchAgainsDrops(parsedString);
+                progressBar1.Value = 100;
+                progressBar1.Visible = false;
+            }
+        }
+
+        /*
+         * Version check and download fresh database after form loaded
+         */
+
+        private void Form1_Shown(object sender, EventArgs e)
+        {
+            if (!backgroundWorker2.IsBusy)
+            {
+                backgroundWorker2.RunWorkerAsync();
+            }
+        }
+
+        private void backgroundWorker2_DoWork(object sender, DoWorkEventArgs e)
+        {
+            getVersion();
+        }
+    }
+}
